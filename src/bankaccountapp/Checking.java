e240fa35e95151f568f66c58bb83e6732658259b
arien <arien@Twist>
2021-03-23 14:00:08 +0200

    Access specifiers updated

--- src/bankaccountapp/Checking.java
+++ src/bankaccountapp/Checking.java
@@ -3,8 +3,8 @@ package bankaccountapp;
 public class Checking extends Account{
 
 	//List properties specific to a Checking account
-	int debitCardNumber;
-	int debitCardPIN;
+	private int debitCardNumber;
+	private int debitCardPIN;
 	
 	//Constructor to initialize Checking account properties
 	
@@ -18,7 +18,8 @@ public class Checking extends Account{
 	
 	@Override
 	public void setRate(){
-		rate = getBaseRate() * .15;
+            System.out.println("Implement rate for Checking");
+            rate = getBaseRate() * .15;
 	}
 	
 	//List any methods specific to a Checking account
@@ -26,8 +27,7 @@ public class Checking extends Account{
 	private void setDebitCard(){
 		debitCardNumber = (int)(Math.random() * Math.pow(10, 12));
 		debitCardPIN = (int)(Math.random() * Math.pow(10, 4));
-		System.out.println("CARD: " + debitCardNumber);
-		System.out.println("PIN: " + debitCardPIN);
+
 	}
 	
 	public void showInfo(){
@@ -38,9 +38,9 @@ public class Checking extends Account{
 				);
 	}
 
-	@Override
+	/*@Override
 	public double getBaseRate() {
 		// TODO Auto-generated method stub
 		return 0;
-	}
+	}*/
 }
